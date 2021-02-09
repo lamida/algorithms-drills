c4c5f35c1f1e05bc33e35ba9e1b0699ef441cf82
Jon Kartago Lamida <me@lamida.net>
2021-02-10 07:44:31 +0800

    Add correct implementation of insertion sort

--- basic_sort.py
+++ basic_sort.py
@@ -6,6 +6,18 @@ def insertion_sort(arr):
                 arr[i], arr[j] = arr[j], arr[i]
         i += 1
 
+# https://www.youtube.com/watch?v=Kg4bqzAqRBM&list=PLUl4u3cNGP61Oq3tWYp6V_F-5jb5L2iHb&index=3&ab_channel=MITOpenCourseWare
+def insertion_sort_mit(arr):
+    key = 1
+    while key < len(arr):
+        j = key
+        while j > 0:
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+            j -= 1
+        key += 1
+
+
 def selection_sort(arr):
     i = 0
     while i < len(arr):
@@ -16,7 +28,10 @@ def selection_sort(arr):
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
         i += 1
 
+
 arr = [2, 10, 1, 0, 7, 1, 2, 100, 1000, -1]
 insertion_sort(arr)
-print(arr)
 
+arr2 = [5, 2, 4, 6, 1, 3]
+insertion_sort_mit(arr2)
+print(arr2)
