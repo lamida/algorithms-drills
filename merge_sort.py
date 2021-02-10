38e480aaa927619db7c236336a2acbfddca8cebd
Indri Rizkina Hapsari <indririzkinahapsari@Indris-iMac.lan>
2021-02-10 21:07:33 +0800

    Fix merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -1,59 +1,56 @@
 # This is ridiculous, but I am still loss in merge sort :(
 from typing import List
-def merge(arr: List[int], m: int):
+
+def merge(arr: List[int]):
+    mid = len(arr)//2
+    i = 0 
+    j = mid + 1
+
     aux = arr.copy()
-    i = 0
-    j = m
-    n = len(arr) - 1
-    print(f"merge. arr: {arr}, m: {m}, i: {i}, j: {j}, n: {n}")
 
-    print(f"range: {list(range(i, n+1))}")
-    for k in range(i, n + 1):
-        if j > n:
-            print(f"index right: {j} is larger than max: {n}, append left to index {k}")
-            aux[k] = arr[i]
-            i += 1
-        elif i > m:
-            print(f"index left: {i} is larger than mid: {m}, append right to index {k}")
-            aux[k] = arr[j]
-            j += 1
-        if arr[i] < arr[j]:
-            print(f"right: {aux[j]} is larger than left: {aux[i]}, append left to index {k}")
-            aux[k] = arr[i]
-            i += 1
-        else:
-            print(f"right: {aux[j]} is smaller than left: {aux[i]}, append right to index {k}")
-            aux[k] = arr[j]
-            j += 1
-    
-    for k in range(i, n + 1):
-        arr[k] = aux[k]
-
-
-def merge2(s1: List[int], s2: List[int], s: List[int]):
-    i = 0
-    j = 0
-    while i + j < len(s):
-        if j == len(s1) or (i < len(s1) and s1[i] < s2[j]):
-            s[i+j] = s1[i]
+    for k in range(len(arr)):
+        if i > mid:
+            arr[k] = aux[j]
+            j+=1
+        elif j > len(arr) - 1: # why len - 1?
+            arr[k] = aux[i]
             i+=1
+        elif aux[j] < aux[i]:
+            arr[k] = aux[j]
+            j+=1
         else:
-            s[i+j] = s2[j]
-            j += 1
-
+            arr[k] = aux[i]
+            i+=1
         
-
 def sort(arr: List[int]):
     if len(arr) > 1:
         mid = len(arr)//2
         sort(arr[:mid])
         sort(arr[mid:])
         print(f"merging: {arr}")
-        merge(arr, mid)
+        merge(arr)
+        print(f"merged: {arr}")
 
+import random
 if __name__ == "__main__":
-    s1 = [100]
-    s2 = [10]
-    s = [None] * 2
-    merge2(s1, s2, s)
-    print(s)
+    # ll = [-30, 13, -43, -7] 
+    # merge(ll)
+    # print(ll)
+    for ll in [[-30, 13, -43, -7, 26, -48, 94, 93]]:
+        print(f"ll is {ll}")
+        c = sorted(ll.copy())
+        sort(ll)
+        if ll != c:
+            print("Found mismatch")
+            print(f"expect: {c}")
+            print(f"but: {ll}")
+    # for i in range(10):
+    #     ll = [random.randint(-100, 100) for i in range(8)]
+    #     print(f"ll is {ll}")
+    #     c = sorted(ll.copy())
+    #     sort(ll)
+    #     if ll != c:
+    #         print("Found mismatch")
+    #         print(f"expect: {c}")
+    #         print(f"but: {ll}")
+
