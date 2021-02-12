66b5fd6a190a4953de70095f3920eaab70744c2a
Jon Kartago Lamida <me@lamida.net>
2021-02-12 08:32:01 +0800

    Finish knapsack tab

--- knapsack.py
+++ knapsack.py
@@ -1,4 +1,5 @@
 # https://www.educative.io/courses/grokking-dynamic-programming-patterns-for-coding-interviews/RM1BDv71V60
+# TODO: Check the knapsack tab to understand it more
 
 from typing import List
 
@@ -43,8 +44,36 @@ def knapsack_memo(profits: List[int], weights: List[int], capacity: int) -> int:
     memo = [[None for x in range (capacity + 1)] for y in range(len(profits))]
     return recurse(profits, weights, capacity, 0, memo, 0)
 
+def knapsack_tab(profits: List[int], weights: List[int], capacity: int) -> int:
+    # list[n][capacity]
+    dp = [[0 for x in range(capacity + 1)] for y in range(len(profits))]
+    n = len(profits)
+
+    # I still don't understand the if condition check here
+    if capacity <= 0 or n == 0 or len(weights) != n:
+        return 0
+    
+    # if we have only one weight, we will take it if it is not more than the capacity
+    # Why???
+    for c in range(0, capacity + 1):
+        if weights[0] <= c:
+            dp[0][c] = profits[0]
+    
+    for i in range(1, n):
+        for c in range(1, capacity + 1):
+            profit1, profit2 = 0, 0
+
+            if weights[i] <= c:
+                profit1 = profits[i] + dp[i - 1][c - weights[i]]
+
+            profit2 = dp[i - 1][c]
+            dp[i][c] = max(profit1, profit2)
+    return dp[n-1][capacity]
+
 if __name__ == "__main__":
     # print(knapsack_memo([1, 6, 10, 16], [1, 2, 3, 5], 7)) # 22
     # print(knapsack_memo([1, 6, 10, 16], [1, 2, 3, 5], 6)) # 17
+    # print(knapsack_memo([1, 6, 10, 16], [1, 2, 3, 5], 5)) # 16
     print(knapsack([1, 6, 10, 16], [1, 2, 3, 5], 7))
     print(knapsack_memo([1, 6, 10, 16], [1, 2, 3, 5], 7))
+    print(knapsack_tab([1, 6, 10, 16], [1, 2, 3, 5], 7))
