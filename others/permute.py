60fbcd5f344d89c5bf4d08a172a10f9fddabbde2
Jon Kartago Lamida <me@lamida.net>
2021-03-02 01:12:32 +0800

    Tidy up the permutation

--- others/permute.py
+++ others/permute.py
@@ -1,15 +1,36 @@
 from typing import List
-l = []
-def permute(a: List[bytearray], l: int, r: int):
-    if l == r:
-        l += [a]
-    else:
-        for i in range(l, r + 1):
-            a[l], a[i] = a[i], a[l]
-            permute(a, l + 1, r)
-            a[l], a[i] = a[i], a[l]
+
+"""
+Just print permutation
+"""
+def permute(s: str):
+    def permute(a: List[str], l: int, r: int):
+        if l == r:
+            print(a)
+        else:
+            for i in range(l, r + 1):
+                a[l], a[i] = a[i], a[l]
+                permute(a, l + 1, r)
+                a[l], a[i] = a[i], a[l]
+    return permute([str(i) for i in "ABC"], 0, len(s) - 1)
+
+"""
+Return the list of permutation
+"""
+def permute2(s: str) -> List[str]:
+    def permute(a: List[str], l: int, r: int, ll: List[List[str]]):
+        if l == r:
+            ll.append("".join(a))
+        else:
+            for i in range(l, r + 1):
+                a[l], a[i] = a[i], a[l]
+                permute(a, l + 1, r, ll)
+                a[l], a[i] = a[i], a[l]
+    ll = []
+    permute([str(i) for i in "ABC"], 0, len(s) - 1, ll)
+    return ll
 
 if __name__ == "__main__":
-    x = permute(bytearray("ABC"), 0, 2)
+    x = permute2("ABC")
     print(x)
 
